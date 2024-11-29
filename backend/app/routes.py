98dad982ea32ade4927adcf76e54879f519dbcde
PPrez24 <eduardo.perez@iteso.mx>
2024-11-29 02:35:06 -0600

    integracion de pdf3

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -17,6 +17,13 @@ sns_client = boto3.client(
     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
 )
 
+s3_client = boto3.client(
+    's3',
+    region_name=os.getenv('AWS_REGION'),
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+)
+
 bp = Blueprint('routes', __name__)
 
 @bp.route('/')
